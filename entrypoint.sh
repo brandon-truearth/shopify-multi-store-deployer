00c87d58c469f3e92404835b2c80e5fc6e29f63d
brandon-truearth <152661801+brandon-truearth@users.noreply.github.com>
2024-07-02 13:56:51 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,6 +73,7 @@ then
 else
         # Checkout specific files from the current commit, ignoring errors
         git checkout ${commit_hash} templates/\index.json 2>/dev/null || true
+        git checkout ${commit_hash} templates/\collection.fundraising.json 2>/dev/null || true
         # git checkout ${commit_hash} sections/\*.json 2>/dev/null || true
         # git checkout ${commit_hash} locales/\*.json 2>/dev/null || true
         git checkout ${commit_hash} config/settings_data.json 2>/dev/null || true
@@ -101,6 +102,7 @@ then
 else
     # Add modified files to the git staging area, ignoring errors
     git add templates/\index.json 2>/dev/null || true
+    git checkout ${commit_hash} templates/\collection.fundraising.json 2>/dev/null || true
     # git add sections/\*.json 2>/dev/null || true
     # git add locales/\*.json 2>/dev/null || true
     git add config/settings_data.json 2>/dev/null || true
